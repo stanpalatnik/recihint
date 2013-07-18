b3453300612ae8ebb1414be73e8dd2ec543f9ed7
Stanislav Palatnik <stan@spalatnik.com>
2013-07-18 19:27:23 -0400

    test

--- bin/Controllers/RecipeController.dart
+++ bin/Controllers/RecipeController.dart
@@ -15,14 +15,13 @@ class RecipeController {
           yield, websitename, serving, recipeurl FROM reciplete.\"Recipe\" where id = @id;""";
       conn.query(query, {'id': request.uri.queryParameters['id']})
       .toList().then((list) { 
-        List recipies = [];
-        list.forEach((row) => 
-            recipies.add(new Recipe(row.id, row.name, row.rating, row.totalTime, row.thumbnail, row.calories, 
-            row.ingredientsList, row.yield, row.serving, row.websitename, row.recipeurl)));
-        completer.complete(json.stringify(recipies));
+        completer.complete(json.stringify(
+            list.map((row) => 
+                new Recipe(row.id, row.name, row.rating, row.totalTime, row.thumbnail, row.calories, 
+                    row.ingredientsList, row.yield, row.serving, row.websitename, row.recipeurl)).toList(growable: false)
+        ));
       }); 
-    });
-    
+    });   
     return completer.future;
   }
   
@@ -34,11 +33,11 @@ var completer = new Completer();
           yield, websitename, serving, recipeurl FROM reciplete.\"Recipe\" where name = @name;""";
       conn.query(query, {'id': request.uri.queryParameters['name']})
       .toList().then((list) { 
-        List recipies = [];
-        list.forEach((row) => 
-            recipies.add(new Recipe(row.id, row.name, row.rating, row.totalTime, row.thumbnail, row.calories, 
-            row.ingredientsList, row.yield, row.serving, row.websitename, row.recipeurl)));
-        completer.complete(json.stringify(recipies));
+        completer.complete(json.stringify(
+            list.map((row) => 
+                new Recipe(row.id, row.name, row.rating, row.totalTime, row.thumbnail, row.calories, 
+                    row.ingredientsList, row.yield, row.serving, row.websitename, row.recipeurl)).toList(growable: false)
+        ));
       }); 
     });
     
